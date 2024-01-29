d197c3dafb554aba1e7bf6f9b5e795bf88284bda
Diogo Muniz <diogomuniz.web@gmail.com>
2024-01-29 04:49:18 -0300

    Alterado nome do banco de dados

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 const DB_HOST = "dbpostech.cd6yk7z7kstc.us-east-1.rds.amazonaws.com";
-const DB_NAME = "dbpostech_pedido";
+const DB_NAME = "dbpostech_produto";
 const DB_USERNAME = "admin";
 const DB_PASSWORD = "Postech123";
 const DB_PORT = 3306;
